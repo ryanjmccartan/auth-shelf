60d3222cede12e434241717fb3d7c85f7c1fdef8
Ryan McCartan <ryanjmccartan@gmail.com>
2019-10-10 14:10:15 -0500

    Added get request for client

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Shelf Page
-    </p>
-  </div>
-);
+class InfoPage extends Component {
+
+state = {
+  shelf: []
+}
+
+componentDidMount() {
+  this.getShelf();
+}
+
+getShelf = () => {
+  axios.get('/api/shelf').then(response => {
+    console.log('GET request', response.data);
+    this.setState({
+      shelf: response.data
+    })
+  }).catch(error => {
+    console.log('Error with GET request client', error);
+  })
+}
+
+
+  render() {
+    return(
+      <p>{JSON.stringify(this.state.shelf)}</p>
+    )
+  }
+}
+
+// const InfoPage = () => (
+//   <div>
+//     <p>
+//       Shelf Page
+//     </p>
+//   </div>
+// );
 
 export default InfoPage;
